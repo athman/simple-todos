b2ddebec462ab9f545497cb410ff87a3bc9de29b
Athman Gude  CS281-0837/2010 <athmangude@gmail.com>
2015-01-04 09:50:21 +0300

    >>Adding user accounts >>App security with methods

--- simple-todos.js
+++ simple-todos.js
@@ -33,10 +33,11 @@ if (Meteor.isClient) {
         "submit .new-task": function(event){
             //This function is called when the new task form is submitted
 
-            Tasks.insert({
-                text: event.target.text.value,
-                createdAt: new Date()
-            });
+            var text = event.target.text.value;
+
+            Meteor.call("addTask", text);
+
+            //console.log(Meteor.user());
 
             //clear the form
             event.target.text.value = "";
@@ -54,11 +55,59 @@ if (Meteor.isClient) {
     Template.task.events({
         "click .toggle-checked": function(){
             //console.log(this);
-            Tasks.update(this._id, {$set: {checked: !this.checked}});
+            Meteor.call("setChecked", this._id, !this.checked);
         },
+
         "click .delete": function(){
-            Tasks.remove(this._id);
+            Meteor.call("deleteTask", this._id);
         }
+
     });
 
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
+    });
 }
+
+Meteor.methods({
+    addTask: function(text){
+        //Make sure the user is logged in before posting
+        if (! Meteor.userId()) {
+            throw new Meteor.Error("Please Sign In");
+        }
+
+        Tasks.insert({
+            text: text,
+            createdAt: new Date(),
+            owner: Meteor.userId(),
+            username: Meteor.user().username
+        });
+    },
+
+    deleteTask: function(taskId){
+        if (! Meteor.userId()) {
+            throw new Meteor.Error("Please Sign In");
+        }
+
+        Tasks.remove(taskId);
+    },
+
+    setChecked: function(taskId, setChecked){
+        if (! Meteor.userId()) {
+            throw new Meteor.Error("Please Sign In");
+        }
+
+        Tasks.update(taskId, {$set: { checked: setChecked} });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
